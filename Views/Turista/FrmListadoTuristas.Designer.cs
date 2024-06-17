fb3edcd2853043b54d36f1a8a3bccb6d8ee98037
GonzaloAgu <gonzalo.ag88@gmail.com>
2024-06-17 19:15:37 -0300

    listado de turistas arreglado

--- Views/Turista/FrmListadoTuristas.Designer.cs
+++ Views/Turista/FrmListadoTuristas.Designer.cs
@@ -29,12 +29,6 @@
         private void InitializeComponent()
         {
             this.TuristasGrd = new System.Windows.Forms.DataGridView();
-            this.DniCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ApellidoCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NombreCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.LocCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Activo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.Moroso = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.ApellidoTxt = new System.Windows.Forms.TextBox();
             this.NombreChk = new System.Windows.Forms.CheckBox();
@@ -43,6 +37,11 @@
             this.PaisCbo = new System.Windows.Forms.ComboBox();
             this.PaisChk = new System.Windows.Forms.CheckBox();
             this.ExportarBtn = new System.Windows.Forms.Button();
+            this.DniCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LocCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Activo = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Moroso = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.TuristasGrd)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -55,13 +54,12 @@
             this.TuristasGrd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TuristasGrd.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.DniCol,
-            this.ApellidoCol,
             this.NombreCol,
             this.LocCol,
             this.Activo,
             this.Moroso});
             this.TuristasGrd.Location = new System.Drawing.Point(9, 69);
-            this.TuristasGrd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TuristasGrd.Margin = new System.Windows.Forms.Padding(2);
             this.TuristasGrd.MultiSelect = false;
             this.TuristasGrd.Name = "TuristasGrd";
             this.TuristasGrd.ReadOnly = true;
@@ -74,6 +72,98 @@
             this.TuristasGrd.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.TuristasGrd_DataBindingComplete);
             this.TuristasGrd.DoubleClick += new System.EventHandler(this.TuristasGrd_DoubleClick);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ApellidoTxt);
+            this.groupBox1.Controls.Add(this.NombreChk);
+            this.groupBox1.Location = new System.Drawing.Point(9, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(227, 42);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // ApellidoTxt
+            // 
+            this.ApellidoTxt.Enabled = false;
+            this.ApellidoTxt.Location = new System.Drawing.Point(69, 15);
+            this.ApellidoTxt.Margin = new System.Windows.Forms.Padding(2);
+            this.ApellidoTxt.Name = "ApellidoTxt";
+            this.ApellidoTxt.Size = new System.Drawing.Size(146, 20);
+            this.ApellidoTxt.TabIndex = 1;
+            // 
+            // NombreChk
+            // 
+            this.NombreChk.AutoSize = true;
+            this.NombreChk.Location = new System.Drawing.Point(4, 17);
+            this.NombreChk.Margin = new System.Windows.Forms.Padding(2);
+            this.NombreChk.Name = "NombreChk";
+            this.NombreChk.Size = new System.Drawing.Size(63, 17);
+            this.NombreChk.TabIndex = 0;
+            this.NombreChk.Text = "Nombre";
+            this.NombreChk.UseVisualStyleBackColor = true;
+            this.NombreChk.CheckedChanged += new System.EventHandler(this.NombreChk_CheckedChanged);
+            // 
+            // FiltroBtn
+            // 
+            this.FiltroBtn.Location = new System.Drawing.Point(719, 18);
+            this.FiltroBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.FiltroBtn.Name = "FiltroBtn";
+            this.FiltroBtn.Size = new System.Drawing.Size(76, 32);
+            this.FiltroBtn.TabIndex = 2;
+            this.FiltroBtn.Text = "Filtrar";
+            this.FiltroBtn.UseVisualStyleBackColor = true;
+            this.FiltroBtn.Click += new System.EventHandler(this.FiltroBtn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.PaisCbo);
+            this.groupBox3.Controls.Add(this.PaisChk);
+            this.groupBox3.Location = new System.Drawing.Point(268, 10);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(225, 41);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            // 
+            // PaisCbo
+            // 
+            this.PaisCbo.DisplayMember = "Id";
+            this.PaisCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PaisCbo.Enabled = false;
+            this.PaisCbo.FormattingEnabled = true;
+            this.PaisCbo.Location = new System.Drawing.Point(90, 15);
+            this.PaisCbo.Margin = new System.Windows.Forms.Padding(2);
+            this.PaisCbo.Name = "PaisCbo";
+            this.PaisCbo.Size = new System.Drawing.Size(115, 21);
+            this.PaisCbo.TabIndex = 1;
+            this.PaisCbo.ValueMember = "Nombre";
+            // 
+            // PaisChk
+            // 
+            this.PaisChk.AutoSize = true;
+            this.PaisChk.Location = new System.Drawing.Point(10, 15);
+            this.PaisChk.Margin = new System.Windows.Forms.Padding(2);
+            this.PaisChk.Name = "PaisChk";
+            this.PaisChk.Size = new System.Drawing.Size(46, 17);
+            this.PaisChk.TabIndex = 0;
+            this.PaisChk.Text = "Pais";
+            this.PaisChk.UseVisualStyleBackColor = true;
+            this.PaisChk.CheckedChanged += new System.EventHandler(this.PaisChk_CheckedChanged);
+            // 
+            // ExportarBtn
+            // 
+            this.ExportarBtn.Enabled = false;
+            this.ExportarBtn.Location = new System.Drawing.Point(736, 458);
+            this.ExportarBtn.Name = "ExportarBtn";
+            this.ExportarBtn.Size = new System.Drawing.Size(84, 26);
+            this.ExportarBtn.TabIndex = 7;
+            this.ExportarBtn.Text = "Exportar";
+            this.ExportarBtn.UseVisualStyleBackColor = true;
+            this.ExportarBtn.Click += new System.EventHandler(this.ExportarBtn_Click);
+            // 
             // DniCol
             // 
             this.DniCol.DataPropertyName = "NroDocumento";
@@ -81,21 +171,12 @@
             this.DniCol.Name = "DniCol";
             this.DniCol.ReadOnly = true;
             // 
-            // ApellidoCol
-            // 
-            this.ApellidoCol.DataPropertyName = "Apellido";
-            this.ApellidoCol.HeaderText = "Apellido";
-            this.ApellidoCol.Name = "ApellidoCol";
-            this.ApellidoCol.ReadOnly = true;
-            this.ApellidoCol.Width = 150;
-            // 
             // NombreCol
             // 
-            this.NombreCol.DataPropertyName = "Nombres";
-            this.NombreCol.HeaderText = "Nombres";
+            this.NombreCol.HeaderText = "Nombre y apellido";
             this.NombreCol.Name = "NombreCol";
             this.NombreCol.ReadOnly = true;
-            this.NombreCol.Width = 150;
+            this.NombreCol.Width = 250;
             // 
             // LocCol
             // 
@@ -121,98 +202,6 @@
             this.Moroso.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Moroso.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.ApellidoTxt);
-            this.groupBox1.Controls.Add(this.NombreChk);
-            this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(227, 42);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            // 
-            // ApellidoTxt
-            // 
-            this.ApellidoTxt.Enabled = false;
-            this.ApellidoTxt.Location = new System.Drawing.Point(69, 15);
-            this.ApellidoTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.ApellidoTxt.Name = "ApellidoTxt";
-            this.ApellidoTxt.Size = new System.Drawing.Size(146, 20);
-            this.ApellidoTxt.TabIndex = 1;
-            // 
-            // NombreChk
-            // 
-            this.NombreChk.AutoSize = true;
-            this.NombreChk.Location = new System.Drawing.Point(4, 17);
-            this.NombreChk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.NombreChk.Name = "NombreChk";
-            this.NombreChk.Size = new System.Drawing.Size(63, 17);
-            this.NombreChk.TabIndex = 0;
-            this.NombreChk.Text = "Nombre";
-            this.NombreChk.UseVisualStyleBackColor = true;
-            this.NombreChk.CheckedChanged += new System.EventHandler(this.NombreChk_CheckedChanged);
-            // 
-            // FiltroBtn
-            // 
-            this.FiltroBtn.Location = new System.Drawing.Point(719, 18);
-            this.FiltroBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.FiltroBtn.Name = "FiltroBtn";
-            this.FiltroBtn.Size = new System.Drawing.Size(76, 32);
-            this.FiltroBtn.TabIndex = 2;
-            this.FiltroBtn.Text = "Filtrar";
-            this.FiltroBtn.UseVisualStyleBackColor = true;
-            this.FiltroBtn.Click += new System.EventHandler(this.FiltroBtn_Click);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.PaisCbo);
-            this.groupBox3.Controls.Add(this.PaisChk);
-            this.groupBox3.Location = new System.Drawing.Point(268, 10);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox3.Size = new System.Drawing.Size(225, 41);
-            this.groupBox3.TabIndex = 4;
-            this.groupBox3.TabStop = false;
-            // 
-            // PaisCbo
-            // 
-            this.PaisCbo.DisplayMember = "Id";
-            this.PaisCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.PaisCbo.Enabled = false;
-            this.PaisCbo.FormattingEnabled = true;
-            this.PaisCbo.Location = new System.Drawing.Point(90, 15);
-            this.PaisCbo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.PaisCbo.Name = "PaisCbo";
-            this.PaisCbo.Size = new System.Drawing.Size(115, 21);
-            this.PaisCbo.TabIndex = 1;
-            this.PaisCbo.ValueMember = "Nombre";
-            // 
-            // PaisChk
-            // 
-            this.PaisChk.AutoSize = true;
-            this.PaisChk.Location = new System.Drawing.Point(10, 15);
-            this.PaisChk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.PaisChk.Name = "PaisChk";
-            this.PaisChk.Size = new System.Drawing.Size(46, 17);
-            this.PaisChk.TabIndex = 0;
-            this.PaisChk.Text = "Pais";
-            this.PaisChk.UseVisualStyleBackColor = true;
-            this.PaisChk.CheckedChanged += new System.EventHandler(this.PaisChk_CheckedChanged);
-            // 
-            // ExportarBtn
-            // 
-            this.ExportarBtn.Enabled = false;
-            this.ExportarBtn.Location = new System.Drawing.Point(736, 458);
-            this.ExportarBtn.Name = "ExportarBtn";
-            this.ExportarBtn.Size = new System.Drawing.Size(84, 26);
-            this.ExportarBtn.TabIndex = 7;
-            this.ExportarBtn.Text = "Exportar";
-            this.ExportarBtn.UseVisualStyleBackColor = true;
-            this.ExportarBtn.Click += new System.EventHandler(this.ExportarBtn_Click);
-            // 
             // FrmListadoTuristas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -223,7 +212,7 @@
             this.Controls.Add(this.FiltroBtn);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.TuristasGrd);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "FrmListadoTuristas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -250,7 +239,6 @@
         private System.Windows.Forms.CheckBox PaisChk;
         private System.Windows.Forms.Button ExportarBtn;
         private System.Windows.Forms.DataGridViewTextBoxColumn DniCol;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ApellidoCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn NombreCol;
         private System.Windows.Forms.DataGridViewTextBoxColumn LocCol;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Activo;
